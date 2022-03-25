700eeeaadb3c6ce177922a686675ae1e972010f4
Alex Krzos <akrzos@users.noreply.github.com>
2022-03-25 15:57:25 -0400

    Adjustments to the monitor script (#104)

--- ansible/roles/rhacm-ztp-setup/templates/monitor-ztp.sh
+++ ansible/roles/rhacm-ztp-setup/templates/monitor-ztp.sh
@@ -15,7 +15,7 @@ log_file=monitor-${start_dt}.log
 
 export KUBECONFIG={{ hub_cluster_kubeconfig }}
 
-echo "date,initialized,booted,discovered,provisioning,install_failed,install_completed,managed,policy_applying,policy_timedout,policy_compliant" > ${csv_file} | tee -a ${log_file}
+echo "date,sno_init,notstarted,booted,discovered,installing,install_failed,install_completed,managed,policy_init,policy_applying,policy_timedout,policy_compliant" > ${csv_file} | tee -a ${log_file}
 
 while true; do
   cur_dt=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
@@ -23,32 +23,36 @@ while true; do
   aci_completed=$(oc get agentclusterinstall -A --no-headers -o custom-columns=NAME:'.metadata.name',COMPLETED:'.status.conditions[?(@.type=="Completed")].reason' 2>> ${log_file})
   cgu_ready=$(oc get clustergroupupgrades -n ztp-install --no-headers -o custom-columns=NAME:'.metadata.name',READY:'.status.conditions[?(@.type=="Ready")].reason' 2>> ${log_file})
 
-  initialized=$(echo "${aci_completed}" | grep -c sno | tr -d " ")
+  sno_init=$(echo "${aci_completed}" | grep -c sno | tr -d " ")
   booted=$(oc get bmh -A --no-headers -o custom-columns=NAME:'.metadata.name',PROVISIONED:'.status.provisioning.state' | grep -c provisioned)
   discovered=$(oc get agent -A --no-headers | wc -l | tr -d " ")
 
-  provisioning=$(echo "${aci_completed}" | grep -c InstallationInProgress | tr -d " ")
+  notstarted=$(echo "${aci_completed}" | grep -c InstallationNotStarted | tr -d " ")
+  installing=$(echo "${aci_completed}" | grep -c InstallationInProgress | tr -d " ")
   install_failed=$(echo "${aci_completed}" | grep -c InstallationFailed | tr -d " ")
   install_completed=$(echo "${aci_completed}" | grep -c InstallationCompleted | tr -d " ")
 
   managed=$(oc get managedcluster -A --no-headers -o custom-columns=JOINED:'.status.conditions[?(@.type=="ManagedClusterJoined")].status',AVAILABLE:'.status.conditions[?(@.type=="ManagedClusterConditionAvailable")].status' | grep -v none | grep -i true | grep -v Unknown | wc -l | tr -d " ")
+  policy_init=$(echo "$cgu_ready" | wc -l)
   policy_applying=$(echo "$cgu_ready" | grep -c UpgradeNotCompleted | tr -d " ")
   policy_timedout=$(echo "$cgu_ready" | grep -c UpgradeTimedOut | tr -d " ")
   policy_compliant=$(echo "$cgu_ready" | grep -c UpgradeCompleted | tr -d " ")
 
   echo "${cur_dt} #########################" | tee -a ${log_file}
-  echo "${cur_dt} initialized: ${initialized}" | tee -a ${log_file}
+  echo "${cur_dt} sno init: ${sno_init}" | tee -a ${log_file}
+  echo "${cur_dt} notstarted: ${notstarted}" | tee -a ${log_file}
   echo "${cur_dt} booted: ${booted}" | tee -a ${log_file}
   echo "${cur_dt} discovered: ${discovered}" | tee -a ${log_file}
-  echo "${cur_dt} provisioning: ${provisioning}" | tee -a ${log_file}
+  echo "${cur_dt} installing: ${installing}" | tee -a ${log_file}
   echo "${cur_dt} install_failed: ${install_failed}" | tee -a ${log_file}
   echo "${cur_dt} install_completed: ${install_completed}" | tee -a ${log_file}
   echo "${cur_dt} managed: ${managed}" | tee -a ${log_file}
+  echo "${cur_dt} policy_init: ${policy_init}" | tee -a ${log_file}
   echo "${cur_dt} policy_applying: ${policy_applying}" | tee -a ${log_file}
   echo "${cur_dt} policy_timedout: ${policy_timedout}" | tee -a ${log_file}
   echo "${cur_dt} policy_compliant: ${policy_compliant}" | tee -a ${log_file}
 
-  echo "${cur_dt},${initialized},${booted},${discovered},${provisioning},${install_failed},${install_completed},${managed},${policy_applying},${policy_timedout},${policy_compliant}" >> ${csv_file} | tee -a ${log_file}
+  echo "${cur_dt},${sno_init},${notstarted},${booted},${discovered},${installing},${install_failed},${install_completed},${managed},${policy_init},${policy_applying},${policy_timedout},${policy_compliant}" >> ${csv_file} | tee -a ${log_file}
 
   sleep ${sleep_period}
 done
