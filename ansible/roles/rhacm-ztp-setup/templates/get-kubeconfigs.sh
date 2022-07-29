1d99434087c331ba0a3da580030143c9cb4c3302
Alex Krzos <akrzos@users.noreply.github.com>
2022-07-29 15:27:45 -0400

    Updates for ACM 2.6 testing (#122)

--- ansible/roles/rhacm-ztp-setup/templates/get-kubeconfigs.sh
+++ ansible/roles/rhacm-ztp-setup/templates/get-kubeconfigs.sh
@@ -3,4 +3,4 @@
 
 export KUBECONFIG={{ hub_cluster_kubeconfig }}
 
-ls /root/hv-sno/manifests/ | xargs -I % sh -c "oc get secret %-admin-kubeconfig -n % -o json | jq -r '.data.kubeconfig' | base64 -d > /root/hv-sno/manifests/%/kubeconfig"
+ls /root/hv-vm/sno/manifests/ | xargs -I % sh -c "oc get secret %-admin-kubeconfig -n % -o json | jq -r '.data.kubeconfig' | base64 -d > /root/hv-vm/sno/manifests/%/kubeconfig"
