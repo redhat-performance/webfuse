67e0349e3124159edba18d179aae267e39e1982f
Alex Krzos <akrzos@redhat.com>
2023-02-20 10:03:28 -0500

    Adjustments to the platform ZTP upgrade process

--- ansible/roles/rhacm-ztp-complete-upgrade-setup/templates/patch-cgu-complete.sh
+++ ansible/roles/rhacm-ztp-complete-upgrade-setup/templates/patch-cgu-complete.sh
@@ -3,6 +3,6 @@
 date -u
 oc --namespace=ztp-platform-upgrade patch clustergroupupgrade.ran.openshift.io/complete-upgrade-{{ du_upgrade_version | replace('.', '-') }}-{{ '%04d' | format(cgu) }} --patch '{"spec":{"enable":true}}' --type=merge
 {% if not loop.last %}
-sleep {{ (complete_upgrade_patch_sleep + complete_upgrade_apply_patch_offset) * 60 }}
+sleep {{ (complete_upgrade_patch_sleep + complete_upgrade_patch_offset) * 60 }}
 {% endif %}
 {% endfor %}
--- ansible/roles/rhacm-ztp-platform-upgrade-setup/templates/patch-cgu-platform.sh
+++ ansible/roles/rhacm-ztp-platform-upgrade-setup/templates/patch-cgu-platform.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 {% for cgu in range(((ztp_done_clusters.stdout_lines | length) / snos_per_cgu) | round(0, 'ceil') | int) %}
-oc --namespace=ztp-platform-upgrade patch clustergroupupgrade.ran.openshift.io/cgu-platform-upgrade-{{ du_upgrade_version | replace('.', '-')  }}-{{ '%04d' | format(cgu) }} --patch '{"spec":{"enable":true}}' --type=merge
+date -u
+oc --namespace=ztp-platform-upgrade patch clustergroupupgrade.ran.openshift.io/platform-upgrade-{{ du_upgrade_version | replace('.', '-')  }}-{{ '%04d' | format(cgu) }} --patch '{"spec":{"enable":true}}' --type=merge
+{% if not loop.last %}
+sleep {{ (platform_upgrade_patch_sleep + platform_upgrade_patch_offset) * 60 }}
+{% endif %}
 {% endfor %}
