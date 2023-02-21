b0d7f53cc6e554d3c5883a4570542ec681495717
Alex Krzos <akrzos@redhat.com>
2023-02-21 10:47:28 -0500

    Use a custom label for cluster selection for upgrades

--- ansible/roles/rhacm-ztp-operator-upgrade-setup/templates/patch-cgu-operator.sh
+++ ansible/roles/rhacm-ztp-operator-upgrade-setup/templates/patch-cgu-operator.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 {% for cgu in range(((operator_update_clusters.stdout_lines | length) / snos_per_cgu) | round(0, 'ceil') | int) %}
-oc --namespace=ztp-operator-upgrade patch clustergroupupgrade.ran.openshift.io/cgu-operator-upgrade-{{ disconnected_operator_index_tag | replace('.', '') }}-{{ '%04d' | format(cgu) }} --patch '{"spec":{"enable":true}}' --type=merge
+oc --namespace=ztp-operator-upgrade patch clustergroupupgrade.ran.openshift.io/operator-upgrade-{{ disconnected_operator_index_tag | replace('.', '') }}-{{ '%04d' | format(cgu) }} --patch '{"spec":{"enable":true}}' --type=merge
 {% endfor %}
